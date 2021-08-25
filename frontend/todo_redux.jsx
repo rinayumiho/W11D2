7826d85589e0be40986e481a6a741e12f0cae3be
Kevin Lin <klin2358@gmail.com>
2021-08-25 14:27:50 -0400

    Phase 1 completed

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,27 @@
+import configureStore from "./store/store";
+import { receiveTodos, receiveTodo, removeTodo } from "./actions/todo_actions";
+import { receiveSteps, receiveStep, removeStep } from "./actions/step_actions";
+// need to remember to import
+import ReactDOM from "react-dom";
+import React from "react";
+import Root from "./components/root"
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to the TODOs.");
+    const store = configureStore();
+    window.store = store;
+    window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
+    window.removeTodo = removeTodo;
+
+    window.receiveSteps = receiveSteps;
+    window.receiveStep = receiveStep;
+    window.removeStep = removeStep;
+
+    const rootElement = document.getElementById("rootElement");
+    ReactDOM.render(<Root store={store}/>, rootElement);
+});
+  
+
+
