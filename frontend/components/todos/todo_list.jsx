4faf9d4dc0a1c5cb2815c615e4954f2d36c39ec5
gamestar <rinayumiho@gmail.com>
2021-08-26 04:55:32 +0800

    temp

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,1 +1,22 @@
-export default () => <h3>Todo List goes here!</h3>
+import React from "react";
+
+// export default () => <div>Todo List goes here!</div>
+
+export const TodoList = (props) => {
+    const todo = props.todos.map((todo) => {
+        return <li key={todo.id}>
+                    title: {todo.title}
+                    <br />
+                    body: {todo.body}
+                </li>
+
+    });
+
+    return (
+        <div>
+          <h1>All Todos</h1>
+          <ul>{TodoList}</ul>
+        </div>
+    );
+    
+} 
--- frontend/components/todos/todo_list_container.jsx
+++ frontend/components/todos/todo_list_container.jsx
@@ -1,9 +1,17 @@
 import { connect } from "react-redux";
+import { receiveTodo } from "../../actions/todo_actions";
 import { allToDos } from "../../reducers/selectors";
 import ToDoList from "./todo_list";
+import React from "react";
 
 const mSTP = state => ({
   //debugger
   todos: allToDos(state)
 });
+
+const mDTP = dispatch => ({
+  receiveTodo: todo => dispatch(receiveTodo(todo))
+});
+
+export default connect(mSTP, mDTP)(ToDoList);
 
